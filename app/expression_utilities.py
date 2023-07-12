ef457cee3c3a349f6187ad4b297227687315b028
KarlLundengaard <k.lundengard@imperial.ac.uk>
2023-07-12 12:31:33 +0100

    Added support for all uppercase names for elementary functions

--- app/expression_utilities.py
+++ app/expression_utilities.py
@@ -26,6 +26,12 @@ elementary_functions_names = [
     ('exp', ['Exp']), ('E', ['e']), ('log', []),
     ('sqrt', []), ('sign', []), ('Abs', ['abs']), ('Max', ['max']), ('Min', ['min']), ('arg', []), ('ceiling', ['ceil']), ('floor', [])
 ]
+for data in elementary_functions_names:
+    upper_case_alternatives = [data[0].upper()]
+    for alternative in data[1]:
+        if alternative.upper() not in upper_case_alternatives:
+            upper_case_alternatives.append(alternative.upper())
+    data[1].extend(upper_case_alternatives)
 
 greek_letters = [
     "Alpha", "alpha", "Beta", "beta", "Gamma", "gamma", "Delta", "delta", "Epsilon", "epsilon", "Zeta", "zeta",
