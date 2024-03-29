5faa486efcc702ae464e99517bbf269eb15ddd44
KarlLundengaard <k.lundengard@imperial.ac.uk>
2024-03-29 11:37:32 +0000

    Added functionality for identifying changing subtraction to addition (and vice versa) and changing sign of an exponent

--- app/symbolic_comparison_evaluation_tests.py
+++ app/symbolic_comparison_evaluation_tests.py
@@ -1101,9 +1101,12 @@ class TestEvaluationFunction():
                         'I': {'aliases': ['i'], 'latex': r'\(i\)'},
                     }
                 }),
-            ("3", "x+1", "response=answer where x=2", True, [], {}),
-            ("6", "x+y+1", "response=answer where x=2; y=3", True, [], {}),
-            ("15", "x+y*z+1", "response=answer where x=2; y=3; z=4", True, [], {}),
+            ("3", "x+1", "response=answer where x=2", True, ["response=answer where x=2_TRUE"], {}),
+            ("1", "x+1", "response=answer where x=2", False, ["response=answer where x=2_ONE_ADDITION_TO_SUBTRACTION", "response candidates x - 1"], {}),
+            ("5/3", "x/y+1", "response=answer where x=2; y=3", True, ["response=answer where x=2; y=3_TRUE"], {}),
+            ("15", "x/y+1", "response=answer where x=2; y=3", False, ["response=answer where x=2; y=3_ONE_EXPONENT_FLIP"], {}), #NOTE: Sympy reporesents input as (x+y)/y so flipping the exponent gives (x+y)*y instead of x*y+1
+            ("-1/3", "x/y+1", "response=answer where x=2; y=3", False, ["response=answer where x=2; y=3_ONE_ADDITION_TO_SUBTRACTION"], {}),
+            ("13", "x+y*z-1", "response=answer where x=2; y=3; z=4", True, [], {}),
         ]
     )
     def test_criteria_based_comparison(self, response, answer, criteria, value, feedback_tags, additional_params):
