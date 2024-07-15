697efb5f9e6b647fcf51da1181a1ba7e4eaef580
annavotin <av1523@ic.ac.uk>
2024-07-15 12:00:47 +0100

    tried to remove dashes from package name in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cmp-exp-data', 
+    name='cmpexpdata', 
     version='0.1.0',  
     packages=find_packages(),  
     install_requires=[
@@ -12,7 +12,7 @@ setup(
         'antlr4-python3-runtime==4.7.2'
     ],
     dependency_links=[
-        'git+https://github.com/lambda-feedback/compareExpressions.git@tr141-temporary-data-analysis-modifications#egg=cmp-exp-data'
+        'git+https://github.com/lambda-feedback/compareExpressions.git@tr141-temporary-data-analysis-modifications#egg=cmpexpdata'
     ],
     long_description=open('README.md').read(),  # Long description from a README file
     long_description_content_type='text/markdown',  # Content type of the long description
