a98390bb3409261c388dd484830afa53bcc7067b
KarlLundengaard <k.lundengard@imperial.ac.uk>
2024-11-22 09:59:47 +0000

    Added flag for generating more detailed feedback when available.

--- app/feedback/symbolic_comparison.py
+++ app/feedback/symbolic_comparison.py
@@ -79,7 +79,7 @@ feedback_generators["response=answer_where"] = lambda tag: lambda inputs: {
 }[tag]
 feedback_generators["IDENTIFY_REASON"] = lambda tag: lambda inputs: {
     "UNKNOWN": None,
-    "ONE_ADDITION_TO_SUBTRACTION": f"{inputs['criterion'].children[0].content_string()} if one addition is changed to a subtraction or vice versa.",
+    "ONE_ADDITION_TO_SUBTRACTION": f"{inputs['criterion'].children[0].content_string()} is true if one addition is changed to a subtraction or vice versa.",
     "ONE_EXPONENT_FLIP": f"{inputs['criterion'].children[0].content_string()} is true if one exponent has its sign changed.",
     "ONE_SWAP_ADDITION_AND_MULTIPLICATION": f"{inputs['criterion'].children[0].content_string()} is true if one addition is replaced with a multiplication or vice versa.",
 }[tag]
