8e0b63efcaf72c4ff95d847e968a393cf7b237ea
Marcus Messer <marcus@marcusm.co.uk>
2025-10-02 10:56:59 +0100

    Added relative_tolerance and absolute_tolerance as alternatives for rtol and atol respectively

--- app/tests/symbolic_evaluation_tests.py
+++ app/tests/symbolic_evaluation_tests.py
@@ -919,6 +919,19 @@ class TestEvaluationFunction():
         result = evaluation_function(response, answer, params)
         assert result["is_correct"] is True
 
+    def test_pi_with_rel_tol(self):
+        answer = "pi"
+        response = "3.14"
+        params = {
+            "strict_syntax": False,
+            "relative_tolerance": 0.05,
+            "symbols": {
+                "pi": {"aliases": ["Pi", "PI", "π"], "latex": "\\(\\pi\\)"},
+            }
+        }
+        result = evaluation_function(response, answer, params)
+        assert result["is_correct"] is True
+
     @pytest.mark.parametrize(
         "response,outcome",
         [
