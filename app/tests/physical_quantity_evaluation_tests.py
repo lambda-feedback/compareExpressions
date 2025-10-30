1c0f835d40f396c643beed515fdf9af6506a5e76
Marcus Messer <marcus@marcusm.co.uk>
2025-10-30 15:37:17 +0000

    Implemented unicode parsing for physical quantities

--- app/tests/physical_quantity_evaluation_tests.py
+++ app/tests/physical_quantity_evaluation_tests.py
@@ -378,7 +378,8 @@ class TestEvaluationFunction():
         "ans,res",
         [
             ("10 ohm", "10 Ω"),
-            ("10 mu A", "10 μ A"),
+            ("10 micro A", "10 μA"),
+            ("10 micro A", "10 μ A"),
             ("30 degree", "30 θ"),
         ]
     )
